5a12e7ac0f7a7d42b5bf81034ac45c0935e07db1
ojwanganto <ojwangwachiaje@gmail.com>
2021-03-17 17:59:00 +0300

    add columns to show appointment duration and honour status on appointments page

--- omod/src/main/java/org/openmrs/module/knh/fragment/controller/KnhUtilsFragmentController.java
+++ omod/src/main/java/org/openmrs/module/knh/fragment/controller/KnhUtilsFragmentController.java
@@ -100,23 +100,21 @@ public class KnhUtilsFragmentController {
 		EncounterType et = encounterService.getEncounterTypeByUuid(HivMetadata._EncounterType.HIV_CONSULTATION);
 		Encounter lastHivGreenCard = patientWrapper.lastEncounter(et);
 		Concept RETURN_VISIT_DATE = Dictionary.getConcept(Dictionary.RETURN_VISIT_DATE);
-		Date returnVisitDate = null;
+		
 		if (lastHivGreenCard != null) {
 			Set<Obs> allObs;
 			allObs = lastHivGreenCard.getAllObs();
 			for (Obs obs : allObs) {
 				if (obs.getConcept().equals(RETURN_VISIT_DATE)) {
 					obsService.voidObs(obs, "Voiding TCA for a new one"); //Voiding the old TCA Date
-					
 					Obs o = new Obs();
 					o.setConcept(Context.getConceptService().getConceptByUuid(Dictionary.RETURN_VISIT_DATE)); // Updating the new TCA Date
 					o.setValueDatetime(tcaDate);
-					//obsService.saveObs(obs, "KenyaEMR Updating Patient TCA");
 					lastHivGreenCard.addObs(o);
 				}
 			}
 		}
-
+		
 		try {
 			encounterService.saveEncounter(lastHivGreenCard);
 			return SimpleObject.create("status", "Success", "message", "TCA for Patient updated successfully");
@@ -125,132 +123,4 @@ public class KnhUtilsFragmentController {
 			return SimpleObject.create("status", "Error", "message", "There was an error updating TCA for Patient");
 		}
 	}
-	
-	/**
-	 * Does the actual assignment of the encounter to a visit
-	 * 
-	 * @param encounter the encounter
-	 * @param newVisitType the type of the new visit if one is created
-	 */
-	protected void assignToVisit(Encounter encounter, VisitType newVisitType) {
-		// Do nothing if the encounter already belongs to a visit and can't be moved
-		if (encounter.getVisit() != null && newVisitType == null) {
-			return;
-		}
-		// Try using an existing visit
-		if (!useExistingVisit(encounter)) {
-			if (newVisitType != null) {
-				useNewVisit(encounter, newVisitType, encounter.getForm());
-			}
-		}
-	}
-	
-	/**
-	 * Uses an existing a visit for the given encounter
-	 * 
-	 * @param encounter the encounter
-	 * @return true if a suitable visit was found
-	 */
-	protected boolean useExistingVisit(Encounter encounter) {
-		// If encounter has time, then we need an exact fit for an existing visit
-		if (EmrUtils.dateHasTime(encounter.getEncounterDatetime())) {
-			List<Visit> visits = Context.getVisitService().getVisits(null,
-			    Collections.singletonList(encounter.getPatient()), null, null, null, encounter.getEncounterDatetime(), null,
-			    null, null, true, false);
-			
-			for (Visit visit : visits) {
-				// Skip visits which ended before the encounter date
-				if (visit.getStopDatetime() != null && visit.getStopDatetime().before(encounter.getEncounterDatetime())) {
-					continue;
-				}
-				
-				if (checkLocations(visit, encounter)) {
-					setVisitOfEncounter(visit, encounter);
-					return true;
-				}
-			}
-		}
-		// If encounter does not have time, we can move it to fit any visit that day
-		else {
-			List<Visit> existingVisitsOnDay = Context.getService(KenyaEmrService.class).getVisitsByPatientAndDay(
-			    encounter.getPatient(), encounter.getEncounterDatetime());
-			if (existingVisitsOnDay.size() > 0) {
-				Visit visit = existingVisitsOnDay.get(0);
-				
-				if (checkLocations(visit, encounter)) {
-					setVisitOfEncounter(visit, encounter);
-					
-					// Adjust encounter start if its before visit start
-					if (encounter.getEncounterDatetime().before(visit.getStartDatetime())) {
-						encounter.setEncounterDatetime(visit.getStartDatetime());
-					}
-					
-					return true;
-				}
-			}
-		}
-		
-		return false;
-	}
-	
-	/**
-	 * Uses a new visit for the given encounter
-	 * 
-	 * @param encounter the encounter
-	 * @param type the visit type
-	 * @param sourceForm the source form
-	 */
-	protected static void useNewVisit(Encounter encounter, VisitType type, Form sourceForm) {
-		Visit visit = new Visit();
-		visit.setStartDatetime(OpenmrsUtil.firstSecondOfDay(encounter.getEncounterDatetime()));
-		visit.setStopDatetime(OpenmrsUtil.getLastMomentOfDay(encounter.getEncounterDatetime()));
-		visit.setLocation(encounter.getLocation());
-		visit.setPatient(encounter.getPatient());
-		visit.setVisitType(type);
-		
-		VisitAttribute sourceAttr = new VisitAttribute();
-		sourceAttr.setAttributeType(MetadataUtils.existing(VisitAttributeType.class,
-		    CommonMetadata._VisitAttributeType.SOURCE_FORM));
-		sourceAttr.setOwner(visit);
-		sourceAttr.setValue(sourceForm);
-		visit.addAttribute(sourceAttr);
-		
-		Context.getVisitService().saveVisit(visit);
-		
-		setVisitOfEncounter(visit, encounter);
-	}
-	
-	/**
-	 * Sets the visit of an encounter, updating the both the old visit and the new visit. This is
-	 * used rather than just encounter.setVisit(...) so that we don't have to reload the visit
-	 * objects to update their set of encounters
-	 * 
-	 * @param visit the visit
-	 * @param encounter the encounter
-	 */
-	protected static void setVisitOfEncounter(Visit visit, Encounter encounter) {
-		// Remove from old visit
-		if (encounter.getVisit() != null) {
-			encounter.getVisit().getEncounters().remove(encounter);
-		}
-		
-		// Set to new visit
-		encounter.setVisit(visit);
-		
-		if (visit != null) {
-			visit.addEncounter(encounter);
-		}
-	}
-	
-	/**
-	 * Convenience method to check whether the location of a visit and an encounter are compatible
-	 * 
-	 * @param visit the visit
-	 * @param encounter the encounter
-	 * @return true if locations won't conflict
-	 */
-	protected static boolean checkLocations(Visit visit, Encounter encounter) {
-		return visit.getLocation() == null || Location.isInHierarchy(encounter.getLocation(), visit.getLocation());
-	}
-	
 }
